09e7fc6e3660fdf09fbb3d6f31f0461c3fdd0515
Swapnil Thomare <59334907+swapnilthomare@users.noreply.github.com>
2023-04-19 10:40:31 +0530

    Update provider_credentials.tf
    key removed

--- 1_basic/provider_credentials.tf
+++ 1_basic/provider_credentials.tf
@@ -6,6 +6,4 @@ provider "myawscloud" {
   region     = "ap-south-1"
  
   profile = "default"  
-  //access_key = "AKIAXBAVDE4KL5EXP3SL"
-  //secret_key = "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"
 }
